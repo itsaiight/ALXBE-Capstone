fb6932dc6ce5512139896429c84c8bdea2c9a1a9
itsaiight <mahlaregosiame@gmail.com>
2025-08-04 21:02:47 +0200

    Update core/models.py by creating all my models

--- profile_and_stats_api/core/models.py
+++ profile_and_stats_api/core/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Driver(models.Model):
+    driver_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=250)
+    number = models.IntegerField()
+    nationality = models.CharField(max_length=100)
+    debut_year = models.IntegerField()
+    current_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    stats = models.CharField()#might change this to a JSONField or similar for complex stats 
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+    
+    def __str__(self):
+        return self.full_name
+    
+class Circuit(models.Model):
+    circuit_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    length_km = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+class Race(models.Model):
+    race_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    round_number = models.IntegerField()
+    year = models.IntegerField()
+    circuit = models.ForeignKey(Circuit, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+    
+class Result(models.Model):
+    result_id = models.AutoField(primary_key=True)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    position = models.IntegerField()
+    points = models.FloatField()
+
+    def __str__(self):
+        return f"{self.driver.full_name} - {self.position} at {self.race.name}"
+    
