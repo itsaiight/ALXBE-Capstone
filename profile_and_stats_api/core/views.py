91f0f8dab7c5c7aebc886ad2cfd4ec97f4fe8706
itsaiight <mahlaregosiame@gmail.com>
2025-08-06 22:31:21 +0200

    Created viewsets for the models

--- profile_and_stats_api/core/views.py
+++ profile_and_stats_api/core/views.py
@@ -1,4 +1,70 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Team, Driver, Circuit, Race, Result
+from .serializers import TeamSerializer, DriverSerializer, CircuitSerializer, RaceSerializer, ResultSerializer
 
 # Create your views here.
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
 
+    def get_queryset(self):
+        return Team.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+
+class DriverViewSet(viewsets.ModelViewSet):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+    def get_queryset(self):
+        return Driver.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+    
+class CircuitViewSet(viewsets.ModelViewSet):
+    queryset = Circuit.objects.all()
+    serializer_class = CircuitSerializer
+
+    def get_queryset(self):
+        return Circuit.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+
+class RaceViewSet(viewsets.ModelViewSet):
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
+
+    def get_queryset(self):
+        return Race.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+
+class ResultViewSet(viewsets.ModelViewSet):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+
+    def get_queryset(self):
+        return Result.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
