b0ce2af6e355a4f6b117f6f97508140d63117376
itsaiight <mahlaregosiame@gmail.com>
2025-08-26 17:11:36 +0200

    Removed all ListApiViews in core views and updated urls accordingly

--- profile_and_stats_api/core/urls.py
+++ profile_and_stats_api/core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TeamViewSet, DriverViewSet, CircuitViewSet, RaceViewSet, ResultViewSet
-from .views import TeamList, DriverList, CircuitList, RaceList, ResultList
 
 router = DefaultRouter()
 router.register(r'teams', TeamViewSet, basename='teams')
@@ -11,10 +10,5 @@ router.register(r'races', RaceViewSet, basename='races')
 router.register(r'results', ResultViewSet, basename='results')
 
 urlpatterns = [
-    path('teams/', TeamList.as_view(), name='team-list'),
-    path('drivers/', DriverList.as_view(), name='driver-list'), 
-    path('races/', RaceList.as_view(), name='race-list'),
-    path('results/', ResultList.as_view(), name='result-list'),
-    path('circuits/', CircuitList.as_view(), name='circuit-list'),
     path('', include(router.urls))
 ]
--- profile_and_stats_api/core/views.py
+++ profile_and_stats_api/core/views.py
@@ -6,9 +6,6 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
-class TeamList(generics.ListAPIView):
-    queryset = Team.objects.all()
-    serializer_class = TeamSerializer
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
@@ -25,11 +22,6 @@ class TeamViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
 
-class DriverList(generics.ListAPIView):
-    queryset = Driver.objects.all()
-    serializer_class = DriverSerializer
-    
-
 class DriverViewSet(viewsets.ModelViewSet):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
@@ -44,10 +36,6 @@ class DriverViewSet(viewsets.ModelViewSet):
         serializer.save()
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
-#might have to create a driverdetailview    
-class CircuitList(generics.ListAPIView):
-    queryset = Circuit.objects.all()
-    serializer_class = CircuitSerializer
 
 class CircuitViewSet(viewsets.ModelViewSet):
     queryset = Circuit.objects.all()
@@ -64,10 +52,6 @@ class CircuitViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
 
-class RaceList(generics.ListAPIView):
-    queryset = Race.objects.all()
-    serializer_class = RaceSerializer
-
 class RaceViewSet(viewsets.ModelViewSet):
     queryset = Race.objects.all()
     serializer_class = RaceSerializer
@@ -82,10 +66,6 @@ class RaceViewSet(viewsets.ModelViewSet):
         serializer.save()
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
-
-class ResultList(generics.ListAPIView):
-    queryset = Result.objects.all()
-    serializer_class = ResultSerializer
 
 class ResultViewSet(viewsets.ModelViewSet):
     queryset = Result.objects.all()
