a36f207125fe3efe9f922736e01366cce907b67c
itsaiight <mahlaregosiame@gmail.com>
2025-08-27 13:22:27 +0200

    Created a permissions viewset to reduce redundent lines of code

--- profile_and_stats_api/core/views.py
+++ profile_and_stats_api/core/views.py
@@ -1,83 +1,35 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 from .models import Team, Driver, Circuit, Race, Result
 from .serializers import TeamSerializer, DriverSerializer, CircuitSerializer, RaceSerializer, ResultSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
+class ReadOnlyOrAdminViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
 
-class TeamViewSet(viewsets.ModelViewSet):
+    def get_permissions(self):
+        if self.request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return [AllowAny()]
+        return [IsAuthenticated(), IsAdminUser()]
+
+class TeamViewSet(ReadOnlyOrAdminViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAdminUser]
 
-    def get_queryset(self):
-        return Team.objects.all()
-    def perform_create(self, serializer):
-        serializer.save()  
-    def perform_update(self, serializer):
-        serializer.save()
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
-
-class DriverViewSet(viewsets.ModelViewSet):
+class DriverViewSet(ReadOnlyOrAdminViewSet):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAdminUser]
 
-    def get_queryset(self):
-        return Driver.objects.all()
-    def perform_create(self, serializer):
-        serializer.save()  
-    def perform_update(self, serializer):
-        serializer.save()
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
-
-class CircuitViewSet(viewsets.ModelViewSet):
+class CircuitViewSet(ReadOnlyOrAdminViewSet):
     queryset = Circuit.objects.all()
     serializer_class = CircuitSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAdminUser]
 
-    def get_queryset(self):
-        return Circuit.objects.all()
-    def perform_create(self, serializer):
-        serializer.save()  
-    def perform_update(self, serializer):
-        serializer.save()
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
-
-class RaceViewSet(viewsets.ModelViewSet):
+class RaceViewSet(ReadOnlyOrAdminViewSet):
     queryset = Race.objects.all()
     serializer_class = RaceSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAdminUser]
 
-    def get_queryset(self):
-        return Race.objects.all()
-    def perform_create(self, serializer):
-        serializer.save()  
-    def perform_update(self, serializer):
-        serializer.save()
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
-
-class ResultViewSet(viewsets.ModelViewSet):
+class ResultViewSet(ReadOnlyOrAdminViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAdminUser]
-
-    def get_queryset(self):
-        return Result.objects.all()
-    def perform_create(self, serializer):
-        serializer.save()  
-    def perform_update(self, serializer):
-        serializer.save()
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
