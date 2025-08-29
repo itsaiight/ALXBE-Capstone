28c872ad878aea2aaaabbcafe2ff05138912c860
Keamogetse <156374079+itsaiight@users.noreply.github.com>
2025-08-29 13:46:04 +0200

    Update views.py
    Added filtering and ordering functionality

--- profile_and_stats_api/core/views.py
+++ profile_and_stats_api/core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Team, Driver, Circuit, Race, Result
 from .serializers import TeamSerializer, DriverSerializer, CircuitSerializer, RaceSerializer, ResultSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from django_filters import rest_framework
+import django_filters
 
 # Create your views here.
 def home(request):
@@ -19,6 +21,15 @@ class ReadOnlyOrAdminViewSet(viewsets.ModelViewSet):
             return [AllowAny()]
         return [IsAuthenticated(), IsAdminUser()]
 
+class DriverFilter(django_filters.FilterSet):
+    class Meta:
+        model = Driver
+        fields = {
+            'full_name': ['icontains'],
+            'debut_year': ['exact', 'gte', 'lte'],
+            'current_team__name': ['icontains'],
+        }
+
 class TeamViewSet(ReadOnlyOrAdminViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
@@ -26,6 +37,14 @@ class TeamViewSet(ReadOnlyOrAdminViewSet):
 class DriverViewSet(ReadOnlyOrAdminViewSet):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+    filterset_class = DriverFilter
+    filter_backends = [
+        rest_framework.DjangoFilterBackend,
+        filters.OrderingFilter]
+
+    search_fields = ['full_name', 'current_team__name']
+    ordering_fields = ['debut_year', 'number', 'full_name']
+    ordering = ['full_name']
 
 class CircuitViewSet(ReadOnlyOrAdminViewSet):
     queryset = Circuit.objects.all()
